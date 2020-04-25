eea86c3ea7614bcf20bc1f5eaca2bc4b1ef95e01
Dmytro Orikhovskyi <d>
2020-04-25 12:09:02 +0300

    add config container.php

--- api/public/index.php
+++ api/public/index.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use Api\Http\Action;
 use Symfony\Component\Dotenv\Dotenv;
 
 chdir(dirname(__DIR__));
@@ -12,10 +11,9 @@ if (file_exists('.env')) {
     (new Dotenv())->load('.env');
 }
 
-$config = require 'config/config.php';
-$container = new \Slim\Container($config);
-$app = new \Slim\App($container);
-
-$app->get('/', Action\HomeAction::class . ':handle');
-
-$app->run();
+(function () {
+    $container = require 'config/container.php';
+    $app = new \Slim\App($container);
+    (require 'config/routes.php')($app);
+    $app->run();
+})();
