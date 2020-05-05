105dbbd97661383253b93369f5f9046a5a9a1331
Dmytro Orikhovskyi <d>
2020-05-05 22:03:26 +0300

    add author action create

--- api/config/common/model.php
+++ api/config/common/model.php
@@ -2,12 +2,14 @@
 
 declare(strict_types=1);
 
+use Api\Infrastructure;
 use Api\Infrastructure\Model\User as UserInfrastructure;
+use Api\Infrastructure\Model\Video as VideoInfrastructure;
 use Api\Model\User as UserModel;
+use Api\Model\Video as VideoModel;
+use Api\ReadModel;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Container\ContainerInterface;
-use Api\Infrastructure;
-use Api\ReadModel;
 
 return [
     Api\Model\Flusher::class => function (ContainerInterface $container) {
@@ -54,8 +56,21 @@ return [
         );
     },
 
+    VideoModel\UseCase\Author\Create\Handler::class => function (ContainerInterface $container) {
+        return new VideoModel\UseCase\Author\Create\Handler(
+            $container->get(Api\Model\Flusher::class),
+            $container->get(VideoModel\Entity\Author\AuthorRepository::class)
+        );
+    },
+
     ReadModel\Video\AuthorReadRepository::class => function (ContainerInterface $container) {
         return new Infrastructure\ReadModel\Video\DoctrineAuthorReadRepository(
+            $container->get(\Doctrine\ORM\EntityManagerInterface::class)
+        );
+    },
+
+    VideoModel\Entity\Author\AuthorRepository::class => function (ContainerInterface $container) {
+        return new VideoInfrastructure\Entity\DoctrineAuthorRepository(
             $container->get(\Doctrine\ORM\EntityManagerInterface::class)
         );
     },
