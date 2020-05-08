b9b0b58b8e0a32b7ffe21660957a8da21fac3d52
Dmytro Orikhovskyi <d>
2020-05-08 13:08:06 +0300

    back action video CreateAction

--- api/config/routes.php
+++ api/config/routes.php
@@ -31,6 +31,7 @@ return function (App $app, ContainerInterface $container) {
     $app->group('/author', function () {
         $this->get('', Action\Author\ShowAction::class . ':handle');
         $this->post('/create', Action\Author\CreateAction::class . ':handle');
+        $this->post('/video/create', Action\Author\Video\CreateAction::class . ':handle');
     })->add($auth);
 
 };
