6d95119e4636b727ae08de92709f59a63f7adccc
Dmytro Orikhovskyi <d>
2020-05-08 15:53:02 +0300

    add VideoUrl

--- api/config/common/app.php
+++ api/config/common/app.php
@@ -7,6 +7,7 @@ use Api\Http\Middleware;
 use Api\Http\Validator\Validator;
 use Api\Model;
 use Api\ReadModel;
+use Api\Http\VideoUrl;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
@@ -86,7 +87,8 @@ return [
     Action\Author\Video\IndexAction::class => function (ContainerInterface $container) {
         return new Action\Author\Video\IndexAction(
             $container->get(ReadModel\Video\AuthorReadRepository::class),
-            $container->get(ReadModel\Video\VideoReadRepository::class)
+            $container->get(ReadModel\Video\VideoReadRepository::class),
+            $container->get(VideoUrl::class)
         );
     },
 
@@ -99,7 +101,8 @@ return [
 
     Action\Author\Video\ShowAction::class => function (ContainerInterface $container) {
         return new Action\Author\Video\ShowAction(
-            $container->get(ReadModel\Video\VideoReadRepository::class)
+            $container->get(ReadModel\Video\VideoReadRepository::class),
+            $container->get(VideoUrl::class)
         );
     },
 ];
--- api/config/common/video.php
+++ api/config/common/video.php
@@ -15,6 +15,7 @@ use Api\Model\Video\Service\Processor\Thumbnailer\Thumbnailer;
 use Api\Model\Video\Service\Uploader;
 use Api\Model\Video\UseCase\Video\Create\Preferences;
 use Psr\Container\ContainerInterface;
+use Api\Http\VideoUrl;
 
 return [
     Uploader::class => function (ContainerInterface $container) {
@@ -57,9 +58,16 @@ return [
         return $preferences;
     },
 
+    VideoUrl::class => function (ContainerInterface $container) {
+        return new VideoUrl(
+            $container->get('config')['video']['base_url']
+        );
+    },
+
     'config' => [
         'video' => [
             'upload_path' => dirname(__DIR__, 3) . '/storage/public/video',
+            'base_url' => getenv('API_STORAGE_URL'),
             'sizes' => [
                 [640, 360],
                 [854, 480],
