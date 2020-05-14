c19992fdf5db8aa4a6909f85e010030fbe8890dd
Dmytro Orikhovskyi <d>
2020-05-14 21:50:00 +0300

    add test commands for rabbit mq

--- api/config/common/console.php
+++ api/config/common/console.php
@@ -5,6 +5,7 @@ use Kafka\ConsumerConfig;
 use Kafka\Producer;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
 
 return [
     Command\Kafka\ProduceCommand::class => function (ContainerInterface $container) {
@@ -19,11 +20,26 @@ return [
         );
     },
 
+
+    Command\Amqp\ProduceCommand::class => function (ContainerInterface $container) {
+        return new Command\Amqp\ProduceCommand(
+            $container->get(AMQPStreamConnection::class)
+        );
+    },
+    Command\Amqp\ConsumeCommand::class => function (ContainerInterface $container) {
+        return new Command\Amqp\ConsumeCommand(
+            $container->get(AMQPStreamConnection::class)
+        );
+    },
+
     'config' => [
         'console' => [
             'commands' => [
                 Command\Kafka\ProduceCommand::class,
                 Command\Kafka\ConsumeCommand::class,
+
+                Command\Amqp\ProduceCommand::class,
+                Command\Amqp\ConsumeCommand::class,
             ],
         ],
     ],
